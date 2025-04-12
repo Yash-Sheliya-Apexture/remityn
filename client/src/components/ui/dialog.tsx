d4134a29012364d6842b709cd99c8a71c735c248
Kartavya-Apexture <kartavya@apexture.in>
2025-04-12 17:52:54 +0530

    Working Send money and add money in sidebar

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -174,7 +174,7 @@ function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50 dark:bg-white/30",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-100 bg-black/50 dark:bg-white/30",
         className
       )}
       {...props}
@@ -193,7 +193,7 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          "bg-white dark:bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 flex flex-col justify-between w-full  translate-x-[-50%] translate-y-[-50%] gap-6 sm:rounded-3xl border sm:p-8 p-4 shadow-lg duration-200 sm:max-w-2xl sm:h-auto h-full",
+          "bg-white dark:bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-120 flex flex-col justify-between w-full  translate-x-[-50%] translate-y-[-50%] gap-6 sm:rounded-3xl border sm:p-8 p-4 shadow-lg duration-200 sm:max-w-2xl sm:h-auto h-full",
           className
         )}
         {...props}
