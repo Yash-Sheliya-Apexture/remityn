d1196b5073f154f15ed66fbab83469dacc72e737
Rudra-Apexture <rudra@apexture.in>
2025-05-09 16:19:24 +0530

    Edit admin-panel changes

--- client/src/app/components/layout/AdminHeader.tsx
+++ client/src/app/components/layout/AdminHeader.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import UserProfile from "@/app/components/UserProfile";
 import { UserProfileProps } from "@/app/components/UserProfile";
 import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 
 interface AdminHeaderProps {
   toggleSidebar: () => void;
@@ -17,21 +18,31 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({ toggleSidebar }) => {
   };
 
   return (
-    <header className="border-b">
+    <header className="bg-white dark:bg-background border-b sticky top-0 z-10">
       {/* Subtle shadow and border */}
-      <div className="p-4 flex justify-between items-center">
-        {/* Mobile Menu Button */}
-        <button
-          onClick={toggleSidebar}
-          className="lg:hidden text-primary hover:text-primaryhover transition-all duration-75 ease-linear focus:outline-none"
-        >
-          <FaBars className="h-6 w-6" /> {/* Smaller menu icon */}
-        </button>
-        <div className="hidden lg:block">
-          {/* You can add header content here if needed for larger screens */}
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between h-20">
+          {/* Mobile Menu Button */}
+          {/* <button
+            onClick={toggleSidebar}
+            className="lg:hidden text-primary hover:text-primaryhover transition-all duration-75 ease-linear focus:outline-none"
+          >
+            <FaBars className="h-6 w-6" />
+          </button> */}
+
+          {/* Close button remains the same */}
+          <button onClick={toggleSidebar} className="size-12 lg:hidden bg-lightgray hover:bg-lightborder dark:bg-primarybox dark:hover:bg-secondarybox flex items-center justify-center rounded-full transition-all duration-75 ease-linear">
+              <span className="text-neutral-900 dark:text-primary">
+                <FaBars size={28} />
+              </span>
+          </button>
+
+          <div className="hidden lg:block">
+            {/* You can add header content here if needed for larger screens */}
+          </div>
+          {/* User Profile Component */}
+          <UserProfile {...adminUserProfileProps} />
         </div>
-        {/* User Profile Component */}
-        <UserProfile {...adminUserProfileProps} />
       </div>
     </header>
   );
