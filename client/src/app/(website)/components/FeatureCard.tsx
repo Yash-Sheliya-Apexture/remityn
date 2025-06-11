22c49837e0a30dbc08acaba05dc63de9909cf867
Yash-Sheliya-Apexture <yash@apexture.in>
2025-06-11 16:15:16 +0530

    add new code

--- client/src/app/(website)/components/FeatureCard.tsx
+++ client/src/app/(website)/components/FeatureCard.tsx
@@ -1,14 +1,13 @@
 // src/components/FeatureCard.tsx
-import React from 'react';
+import { AudioWaveform, Clock, Send, Shuffle, Wallet } from "lucide-react";
+import React from "react";
 // Import appropriate icons from react-icons, as they are used in the features array
 import {
-  FaUserPlus,      // For Account Setup
-  FaWallet,        // For Fund Your Account
-  FaPaperPlane,    // For Direct Transfers
-  FaExchangeAlt,   // For Exchange Rates
-  FaShieldAlt,     // For Security
-  FaReceipt,       // For Transparent Fees
-  FaHeadset,       // For Customer Support
+  FaUserPlus, // For Account Setup
+  FaWallet, // For Fund Your Account
+  FaPaperPlane, // For Direct Transfers
+  FaExchangeAlt, // For Exchange Rates
+  FaHeadset, // For Customer Support
 } from "react-icons/fa";
 
 // Interface for a single feature item
@@ -25,58 +24,42 @@ export interface Feature {
 export const features: Feature[] = [
   {
     id: 1,
-    icon: FaUserPlus,
-    title: "Quick & Easy Account Setup",
-    description: "Create your account in minutes and start sending money without delays.",
+    icon: Clock,
+    title: "2-Minute Sign-Up",
+    description: "Snap your ID, you’re in.",
     colSpanLarge: "lg:col-span-3", // Spans 3 of 6 columns on large screens
     iconBgColor: "#e3ffd1", // Light green
   },
   {
     id: 2,
-    icon: FaWallet,
-    title: "Fund Your Account Your Way",
-    description: "Easily add funds to your account balance using your local currency via bank transfer.",
+    icon: Wallet,
+    title: "Pay Any Way",
+    description: "Use bank transfer.",
     colSpanLarge: "lg:col-span-3", // Spans 3 of 6 columns on large screens
     iconBgColor: "#9bf7ff", // Light blue
   },
   {
     id: 3,
-    icon: FaPaperPlane,
-    title: "Instant Transfers to Indian Accounts",
-    description: "Send money from your wallet to a recipient's bank account in India,, efficiently and securely.",
+    icon: Send,
+    title: "Instant INR Deposit",
+    description: "Money drops via Bank transfer.",
     colSpanLarge: "lg:col-span-2", // Spans 2 of 6 columns on large screens
     iconBgColor: "#e2c9fb", // Light pink
   },
   {
     id: 4,
-    icon: FaExchangeAlt,
-    title: "Competitive Exchange Rates",
-    description: "Enjoy exchange rates that are better than the market average.",
+    icon: Shuffle,
+    title: "Live Exchange Rate",
+    description: "See it, lock it, send it.",
     colSpanLarge: "lg:col-span-2", // Spans 2 of 6 columns on large screens
     iconBgColor: "#fbcdcd", // Light orange
   },
   {
-    id: 5,
-    icon: FaShieldAlt,
-    title: "In-House, Enterprise-Grade Security",
-    description: "Our in-house security ensures the highest standards of data protection and fraud prevention.",
-    colSpanLarge: "lg:col-span-2", // Spans 2 of 6 columns on large screens
-    iconBgColor: "#fee09f", // Light purple
-  },
-  {
-    id: 6,
-    icon: FaReceipt,
-    title: "Transparent Fees & Process",
-    description: "Know exactly what you pay with upfront fees and full transfer tracking.",
-    colSpanLarge: "lg:col-span-3", // Spans 3 of 6 columns on large screens
-    iconBgColor: "#98FB98", // Light cyan
-  },
-  {
     id: 7,
-    icon: FaHeadset,
-    title: "24/7 Customer Support",
-    description: "Our dedicated support team is available 24/7 to assist with any queries or issues.",
-    colSpanLarge: "lg:col-span-3", // Spans 3 of 6 columns on large screens
+    icon: AudioWaveform,
+    title: "Clear Tracking",
+    description: "Watch every step till it arrives.",
+    colSpanLarge: "lg:col-span-2", // Spans 3 of 6 columns on large screens
     iconBgColor: "#FFA07A", // Light yellow
   },
 ];
@@ -97,20 +80,30 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
   iconBgColor,
 }) => {
   return (
-    <div className="bg-background relative w-full h-full rounded-3xl p-4 md:p-6 flex flex-col gap-12">
-      
-        <div
-          className="lg:w-18 w-14 lg:h-18 h-14 xl:rounded-2xl rounded-xl flex items-center justify-center"
-          style={{ backgroundColor: iconBgColor }}
-        >
-          <Icon className="text-mainheading transition-colors duration-300 ease-in-out xl:size-6 size-5" />
-        </div>
-      
+    <div className="bg-background relative w-full h-full rounded-3xl p-4 md:p-6 flex flex-col gap-12 overflow-hidden">
+      <div className="absolute -right-1 -top-1">
+        <img
+          alt=""
+          loading="lazy"
+          width="150"
+          height="150"
+          decoding="async"
+          data-nimg="1"
+          className="z-1"
+          src="/assets/images/RightShep.svg"
+        />
+      </div>
+      <div
+        className="lg:w-16 w-14 lg:h-16 h-14 xl:rounded-2xl rounded-xl flex items-center justify-center"
+        style={{ backgroundColor: iconBgColor }}
+      >
+        <Icon className="text-mainheading xl:size-7 size-6" />
+      </div>
       <div className="flex flex-col flex-grow xl:justify-end">
         <h3 className=" text-mainheadingWhite group-hover:text-primary text-2xl font-semibold line-clamp-2 mb-2">
           {title}
         </h3>
-        <p className="text-subheadingWhite lg:text-lg text-base">
+        <p className="text-subheadingWhite/60 lg:text-lg text-base">
           {description}
         </p>
       </div>
