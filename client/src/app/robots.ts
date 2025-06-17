b8d03d8c42b03fe84eb5d4731b4382a765a1ad27
Yash-Sheliya-Apexture <yash@apexture.in>
2025-06-17 13:53:45 +0530

    add new code

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -1,11 +1,27 @@
-import type { MetadataRoute } from 'next'
+// import type { MetadataRoute } from 'next'
  
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: {
+//       userAgent: '*',
+//       allow: '*',
+//       // Corrected: Removed space before /_next/
+//       disallow: ['/api/','/auth/', '/admin/', '/dashboard/', '/add-money/', '/send-money/', '/transactions/', '/your-account/', '/kyc/', '/_next/' ],
+//     },
+//     sitemap: 'https://www.remityn.com/sitemap.xml',
+//   }
+// }
+
+// robots.ts
+
+import type { MetadataRoute } from 'next' 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '*',
-      // Corrected: Removed space before /_next/
+      // Change ' /_next/' to '/_next/'
       disallow: ['/api/','/auth/', '/admin/', '/dashboard/', '/add-money/', '/send-money/', '/transactions/', '/your-account/', '/kyc/', '/_next/' ],
     },
     sitemap: 'https://www.remityn.com/sitemap.xml',
