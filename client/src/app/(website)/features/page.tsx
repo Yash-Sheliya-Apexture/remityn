d2baed827c91e94872f8f9430633ea688f2c668c
Kartavya-Apexture <kartavya@apexture.in>
2025-06-24 19:12:04 +0530

    error solve

--- client/src/app/(website)/features/page.tsx
+++ client/src/app/(website)/features/page.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import FeatureHero from "../components/Feature/FeatureHero";
 import CallToActionSection from "../components/home/CallToActionSection";
 import HowItWorksSection from "../components/Feature/HowItWorksSection";
-import ChooseFeature from "../components/ChooseFeature";
 import TransferWaySection from "../components/Feature/TransferWaySection";
-import WhyUsSection from "../components/WhyUsSection";
 
 const page = () => {
   return (
     <div className="Features-Main">
-      {/* <ChooseFeature /> */}
       <FeatureHero />
-      {/* <SendMoneySteps /> */}
-      {/* <WhyUsSection /> */}
       <HowItWorksSection />
       <TransferWaySection />
       <CallToActionSection />
